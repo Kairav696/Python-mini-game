a84bdc8bc27dc9bcf609d2e66e9fd187c63eee1d
Kairav696 <kairavsinghm@gmail.com>
2026-02-02 22:25:38 +0530

    Add files via upload

---
+++ Day 5.py
@@ -0,0 +1,14 @@
+secret_number = 7
+guess = 0
+
+print("Guess the number between 1 and 10")
+
+while guess != secret_number:
+    guess = int(input("Enter your guess: "))
+
+    if guess < secret_number:
+        print("Too low, try again")
+    elif guess > secret_number:
+        print("Too high, try again")
+    else:
+        print("Correct! You guessed the number 🎉")
